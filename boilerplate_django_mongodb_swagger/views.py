235d5a23d76b068e25689bfdb384727d3e6e2f1a
Quang Khang <bicendinh.dev@gmail.com>
2024-06-24 14:46:24 +0700

    add swagger & .gitignore

---
+++ boilerplate_django_mongodb_swagger/views.py
@@ -0,0 +1,8 @@
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class ExampleView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
